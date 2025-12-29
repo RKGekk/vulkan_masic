e3e0ce3957c523d40479a475aca9b12becad60cc
o.arkhangelsky <o.arkhangelsky@vk.team>
2025-12-29 20:41:06 +0300

    scene nodes debug UI WIP2

--- src/engine/screen_elements/node_menu_ui.cpp
+++ src/engine/screen_elements/node_menu_ui.cpp
@@ -4,6 +4,10 @@
 #include "../base_engine_logic.h"
 #include "../../scene/scene.h"
 #include "../views/human_view.h"
+#include "../../scene/nodes/scene_node.h"
+#include "../../scene/nodes/mesh_node.h"
+#include "../../scene/nodes/camera_node.h"
+#include "../../scene/nodes/aabb_node.h"
 
 NodeMenuUI::NodeMenuUI() {
 }
@@ -58,25 +62,128 @@ bool NodeMenuUI::VOnRender(const GameTimerDelta& delta) {
                                    + " "s + node_type_flags_str
                                    + " "s + node_name;
                 if (ImGui::TreeNode(header.c_str())) {
-                    ImGui::SeparatorText("Hierarchy");
-                    if (ImGui::BeginTable("Hierarchy Table", 3)) {
-                        ImGui::TableNextRow();
+                    if (ImGui::TreeNode("Hierarchy")) {
+						if (ImGui::BeginTable("Hierarchy Table", 3)) {
+                            ImGui::TableNextRow();
+                            ImGui::TableSetColumnIndex(0);
+
+                            int parent = hierarchy_node.parent;
+		                    ImGui::InputInt("Parent", &parent, 0, 0, ImGuiInputTextFlags_ReadOnly);
+
+                            int first_child = hierarchy_node.first_child;
+                            ImGui::InputInt("Child", &first_child, 0, 0, ImGuiInputTextFlags_ReadOnly);
+
+                            int next_sibling = hierarchy_node.next_sibling;
+                            ImGui::InputInt("Sibling", &next_sibling, 0, 0, ImGuiInputTextFlags_ReadOnly);
+
+                            int level = hierarchy_node.level;
+                            ImGui::InputInt("Level", &level, 0, 0, ImGuiInputTextFlags_ReadOnly);
+                            
+                            ImGui::EndTable();
+                        }
                         
-                        ImGui::TableSetColumnIndex(0);
-                        int parent = hierarchy_node.parent;
-		                ImGui::InputInt("Parent", &parent, 0, 0, ImGuiInputTextFlags_ReadOnly);
-
-                        int first_child = hierarchy_node.first_child;
-                        ImGui::InputInt("Child", &first_child, 0, 0, ImGuiInputTextFlags_ReadOnly);
-
-                        int next_sibling = hierarchy_node.next_sibling;
-                        ImGui::InputInt("Sibling", &next_sibling, 0, 0, ImGuiInputTextFlags_ReadOnly);
-
-                        int level = hierarchy_node.level;
-                        ImGui::InputInt("Level", &level, 0, 0, ImGuiInputTextFlags_ReadOnly);
+						ImGui::TreePop();
+					}
+                    
+                    if (ImGui::TreeNode("Properties")) {
                         
-                        ImGui::EndTable();
-                    }
+                        ImGui::SeparatorText("Properties");
+
+                        if(node_name_map.contains(node_index)) {
+                            std::string node_name = node_names.at(node_name_map.at(node_index)).c_str();
+                            ImGui::InputText("Name", const_cast<char*>(node_name.c_str()), 128, ImGuiInputTextFlags_ReadOnly);
+                        }
+
+                        if(scene->getNodeTypeFlagsMap().contains(node_index)) {
+                            std::string node_type_flags_raw = std::to_string(node_type_flags);
+                            std::string node_type_v = node_type_flags_raw + " --> " + node_type_flags_str;
+                            ImGui::InputText("Type Flags", const_cast<char*>(node_type_v.c_str()), 128, ImGuiInputTextFlags_ReadOnly);
+                        }
+
+                        ImGui::SeparatorText("Transforms");
+
+                        if (ImGui::TreeNode("Local Transform")) {
+        					if (ImGui::InputFloat4("R1", ((float*)&scene->getNodeLocalTransform(node_index)) + 0, "%.4f", ImGuiInputTextFlags_ReadOnly)) {}
+        					if (ImGui::InputFloat4("R2", ((float*)&scene->getNodeLocalTransform(node_index)) + 4, "%.4f", ImGuiInputTextFlags_ReadOnly)) {}
+        					if (ImGui::InputFloat4("R3", ((float*)&scene->getNodeLocalTransform(node_index)) + 8, "%.4f", ImGuiInputTextFlags_ReadOnly)) {}
+        					if (ImGui::InputFloat4("R4", ((float*)&scene->getNodeLocalTransform(node_index)) + 12, "%.4f", ImGuiInputTextFlags_ReadOnly)) {}
+        					ImGui::TreePop();
+        				}
+
+                        if (ImGui::TreeNode("Global Transform")) {
+        					if (ImGui::InputFloat4("R1", ((float*)&scene->getNodeGlobalTransform(node_index)) + 0, "%.4f", ImGuiInputTextFlags_ReadOnly)) {}
+        					if (ImGui::InputFloat4("R2", ((float*)&scene->getNodeGlobalTransform(node_index)) + 4, "%.4f", ImGuiInputTextFlags_ReadOnly)) {}
+        					if (ImGui::InputFloat4("R3", ((float*)&scene->getNodeGlobalTransform(node_index)) + 8, "%.4f", ImGuiInputTextFlags_ReadOnly)) {}
+        					if (ImGui::InputFloat4("R4", ((float*)&scene->getNodeGlobalTransform(node_index)) + 12, "%.4f", ImGuiInputTextFlags_ReadOnly)) {}
+        					ImGui::TreePop();
+        				}
+
+                        if (ImGui::TreeNode("Decompose Local")) {
+					    	glm::vec3 scale_xm;
+					    	glm::quat rotation_xm;
+					    	glm::vec3 translation_xm;
+
+                            glm::mat4x4 mat = scene->getNodeLocalTransform(node_index);
+                            translation_xm = mat[3];
+
+                            for (int i = 0; i < 3; ++i) {
+                                scale_xm[i] = glm::length(mat[i]);
+                                mat[i] /= scale_xm[i];
+                            }
+                        
+                            rotation_xm = glm::toQuat(mat);
+
+					    	glm::vec3 pyr_xm = glm::eulerAngles(rotation_xm);
+					    	glm::vec3 ypr_xm(
+					    		glm::degrees(pyr_xm.y),
+					    		glm::degrees(pyr_xm.x),
+					    		glm::degrees(pyr_xm.z)
+					    	);
+
+					    	if (ImGui::InputFloat4("Rq", ((float*)&rotation_xm), "%.4f", ImGuiInputTextFlags_ReadOnly)) {}
+					    	if (ImGui::InputFloat3("Sc", ((float*)&scale_xm), "%.4f", ImGuiInputTextFlags_ReadOnly)) {}
+					    	if (ImGui::InputFloat3("Tr", ((float*)&translation_xm), "%.4f", ImGuiInputTextFlags_ReadOnly)) {}
+					    	if (ImGui::InputFloat3("Ypr", ((float*)&ypr_xm), "%.4f", ImGuiInputTextFlags_ReadOnly)) {}
+					    	
+					    	ImGui::TreePop();
+					    }
+
+                        if (ImGui::TreeNode("Decompose Global")) {
+					    	glm::vec3 scale_xm;
+					    	glm::quat rotation_xm;
+					    	glm::vec3 translation_xm;
+
+                            glm::mat4x4 mat = scene->getNodeGlobalTransform(node_index);
+                            translation_xm = mat[3];
+
+                            for (int i = 0; i < 3; ++i) {
+                                scale_xm[i] = glm::length(mat[i]);
+                                mat[i] /= scale_xm[i];
+                            }
+                        
+                            rotation_xm = glm::toQuat(mat);
+
+					    	glm::vec3 pyr_xm = glm::eulerAngles(rotation_xm);
+					    	glm::vec3 ypr_xm(
+					    		glm::degrees(pyr_xm.y),
+					    		glm::degrees(pyr_xm.x),
+					    		glm::degrees(pyr_xm.z)
+					    	);
+
+					    	if (ImGui::InputFloat4("Rq", ((float*)&rotation_xm), "%.4f", ImGuiInputTextFlags_ReadOnly)) {}
+					    	if (ImGui::InputFloat3("Sc", ((float*)&scale_xm), "%.4f", ImGuiInputTextFlags_ReadOnly)) {}
+					    	if (ImGui::InputFloat3("Tr", ((float*)&translation_xm), "%.4f", ImGuiInputTextFlags_ReadOnly)) {}
+					    	if (ImGui::InputFloat3("Ypr", ((float*)&ypr_xm), "%.4f", ImGuiInputTextFlags_ReadOnly)) {}
+					    	
+					    	ImGui::TreePop();
+					    }
+
+                        ImGui::SeparatorText("Node Types");
+
+
+                        
+						ImGui::TreePop();
+					}
 
 					ImGui::TreePop();
 				}
