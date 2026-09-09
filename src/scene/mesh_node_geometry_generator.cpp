3265db9f40fcd5d70bc7dbbd6c7151d360e6de17
o.arkhangelsky <o.arkhangelsky@vk.team>
2026-09-09 19:09:47 +0300

    fix draw bones

--- src/scene/mesh_node_geometry_generator.cpp
+++ src/scene/mesh_node_geometry_generator.cpp
@@ -82,21 +82,83 @@ std::shared_ptr<SceneNode> MeshNodeGeometryGenerator::GenerateBoneLine(const std
         if(!root_bone) continue;
         const BoneNode::BoneData bone_data = root_bone->getBoneDataMap().at(skeleton_name);
         glm::mat4 model_from_root = bone_data.mesh_root_node->Get().FromRoot();
-        glm::mat4 root_transform = glm::mat4(1.0f);
+        //glm::mat4 root_transform = glm::mat4(1.0f);
 
         std::stack<std::shared_ptr<BoneNode>> node_stack;
         node_stack.push(root_bone);
         while(!node_stack.empty()) {
             std::shared_ptr<BoneNode> current_bone = node_stack.top();
-            const Scene::Hierarchy& current_bone_hierarchy = m_scene->getNodeHierarchy(current_bone->VGetNodeIndex());
             node_stack.pop();
+            if(!current_bone) continue;
+            const Scene::Hierarchy& current_bone_hierarchy = m_scene->getNodeHierarchy(current_bone->VGetNodeIndex());
 
-            if(const std::shared_ptr<SceneNode>& sibling_node = m_scene->getProperty(current_bone_hierarchy.next_sibling, Scene::NODE_TYPE_FLAG_BONE)) {
-                node_stack.push(std::dynamic_pointer_cast<BoneNode>(sibling_node));
+            const std::shared_ptr<SceneNode>& sibling_node = m_scene->getProperty(current_bone_hierarchy.next_sibling, Scene::NODE_TYPE_FLAG_BONE);
+            if(sibling_node) {
+                std::shared_ptr<BoneNode> sibling_bone = std::dynamic_pointer_cast<BoneNode>(sibling_node);
+                node_stack.push(sibling_bone);
+
+                const std::shared_ptr<SceneNode>& parent_node = m_scene->getProperty(current_bone_hierarchy.parent, Scene::NODE_TYPE_FLAG_BONE);
+                std::shared_ptr<BoneNode> parent_bone = std::dynamic_pointer_cast<BoneNode>(parent_node);
+                if(skin_data->bone_to_joint_map.contains(sibling_bone) && parent_node && skin_data->bone_to_joint_map.contains(parent_bone)) {
+
+                    const glm::mat4& to_root0 = sibling_bone->Get().ToRoot();
+                    glm::mat4 final_sceleton_space0 = model_from_root * to_root0;
+                    glm::vec3 pos0 = glm::vec3(final_sceleton_space0[3]);
+                    glm::vec4 color0 = glm::vec4(1.0f, 0.0f, 0.0f, 0.5f);
+
+                    const glm::mat4& to_root1 = parent_bone->Get().ToRoot();
+                    glm::mat4 final_sceleton_space1 = model_from_root * to_root1;
+                    glm::vec3 pos1 = glm::vec3(final_sceleton_space1[3]);
+                    glm::vec4 color1 = glm::vec4(1.0f, 0.0f, 0.0f, 0.5f);
+
+                    size_t line_start = b * vertex_stride * vertices_per_line;
+
+                    size_t vertex0_index = line_start + 0u * 0u;
+                    size_t vertex1_index = line_start + next_vertex_start * 1u;
+                    size_t vertex2_index = line_start + next_vertex_start * 2u;
+                    size_t vertex3_index = line_start + next_vertex_start * 3u;
+
+                    for (size_t current_component_num = 0; current_component_num < pos_num_of_elements_to_copy; ++current_component_num) {
+                        vertex_data[vertex0_index + pos_offset + current_component_num] = pos0[current_component_num];
+                        vertex_data[vertex1_index + pos_offset + current_component_num] = pos0[current_component_num];
+                        vertex_data[vertex2_index + pos_offset + current_component_num] = pos1[current_component_num];
+                        vertex_data[vertex3_index + pos_offset + current_component_num] = pos1[current_component_num];
+                    }
+                    for (size_t current_component_num = 0; current_component_num < color_num_of_elements_to_copy; ++current_component_num) {
+                        vertex_data[vertex0_index + color_offset + current_component_num] = color0[current_component_num];
+                        vertex_data[vertex1_index + color_offset + current_component_num] = color0[current_component_num];
+                        vertex_data[vertex2_index + color_offset + current_component_num] = color1[current_component_num];
+                        vertex_data[vertex3_index + color_offset + current_component_num] = color1[current_component_num];
+                    }
+                    for (size_t current_component_num = 0; current_component_num < target_num_of_elements_to_copy; ++current_component_num) {
+                        vertex_data[vertex0_index + target_offset + current_component_num] = pos1[current_component_num];
+                        vertex_data[vertex1_index + target_offset + current_component_num] = pos1[current_component_num];
+                        vertex_data[vertex2_index + target_offset + current_component_num] = pos0[current_component_num];
+                        vertex_data[vertex3_index + target_offset + current_component_num] = pos0[current_component_num];
+                    }
+                    for (size_t current_component_num = 0; current_component_num < side_num_of_elements_to_copy; ++current_component_num) {
+                        vertex_data[vertex0_index + side_offset + current_component_num] = -1.0f;
+                        vertex_data[vertex1_index + side_offset + current_component_num] = 1.0f;
+                        vertex_data[vertex2_index + side_offset + current_component_num] = -1.0f;
+                        vertex_data[vertex3_index + side_offset + current_component_num] = 1.0f;
+                    }
+
+                    indices.push_back(0 + b * vertices_per_line);
+                    indices.push_back(1 + b * vertices_per_line);
+                    indices.push_back(2 + b * vertices_per_line);
+                    indices.push_back(2 + b * vertices_per_line);
+                    indices.push_back(0 + b * vertices_per_line);
+                    indices.push_back(3 + b * vertices_per_line);
+
+                    ++b;
+                }
             }
 
-            if(const std::shared_ptr<SceneNode>& child_node = m_scene->getProperty(current_bone_hierarchy.first_child, Scene::NODE_TYPE_FLAG_BONE)) {
+            const std::shared_ptr<SceneNode>& child_node = m_scene->getProperty(current_bone_hierarchy.first_child, Scene::NODE_TYPE_FLAG_BONE);
+            if(child_node) {
                 std::shared_ptr<BoneNode> child_bone = std::dynamic_pointer_cast<BoneNode>(child_node);
+                node_stack.push(child_bone);
+
                 if(skin_data->bone_to_joint_map.contains(child_bone)) {
                     const glm::mat4& to_root0 = current_bone->Get().ToRoot();
                     glm::mat4 final_sceleton_space0 = model_from_root * to_root0;
@@ -140,17 +202,20 @@ std::shared_ptr<SceneNode> MeshNodeGeometryGenerator::GenerateBoneLine(const std
                         vertex_data[vertex3_index + side_offset + current_component_num] = 1.0f;
                     }
 
-                    indices.push_back(0 + b * vertices_per_line * vertices_per_line);
-                    indices.push_back(1 + b * vertices_per_line * vertices_per_line);
-                    indices.push_back(2 + b * vertices_per_line * vertices_per_line);
-                    indices.push_back(2 + b * vertices_per_line * vertices_per_line);
-                    indices.push_back(0 + b * vertices_per_line * vertices_per_line);
-                    indices.push_back(3 + b * vertices_per_line * vertices_per_line);
-                }
+                    indices.push_back(0 + b * vertices_per_line);
+                    indices.push_back(1 + b * vertices_per_line);
+                    indices.push_back(2 + b * vertices_per_line);
+                    indices.push_back(2 + b * vertices_per_line);
+                    indices.push_back(0 + b * vertices_per_line);
+                    indices.push_back(3 + b * vertices_per_line);
 
-                node_stack.push(std::move(child_bone));
+                    ++b;
+                }
             }
-            ++b;
+            
+            // if(!sibling_node && !child_node) {
+            //     std::string ff = ""s;
+            // }
         }
     }
 
